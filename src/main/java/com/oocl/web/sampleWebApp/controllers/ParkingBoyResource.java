7665df991550481fe25aefa80036d2dc2e8bef3d
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-07 10:07:01 +0800

    first commit with simple GET

---
+++ src/main/java/com/oocl/web/sampleWebApp/controllers/ParkingBoyResource.java
@@ -0,0 +1,15 @@
+package com.oocl.web.sampleWebApp.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/parkingboys")
+public class ParkingBoyResource {
+
+    @GetMapping
+    public ResponseEntity<String[]> getAll() {
+        return ResponseEntity.ok(new String[]{"I", "am", "a", "new", "array"});
+    }
+
+}
---
+++ src/main/java/com/oocl/web/sampleWebApp/domain/ParkingBoyRepository.java
@@ -0,0 +1,9 @@
+package com.oocl.web.sampleWebApp.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParkingBoyRepository extends JpaRepository<ParkingBoy, Long> {
+    ParkingBoy findFirstByEmployeeId(String employeeId);
+}
