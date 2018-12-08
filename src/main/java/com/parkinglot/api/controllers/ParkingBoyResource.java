1d0041efd149379cbd982c1628002dde3d4ece06
Mo Hon Sing <mohonsing113@gmail.com>
2018-12-08 19:55:13 +0800

    [Mo] feat: add PUT method for parking car to parking lot, and return order as Response

--- src/main/java/com/parkinglot/api/controllers/ParkingBoyResource.java
+++ src/main/java/com/parkinglot/api/controllers/ParkingBoyResource.java
@@ -1,9 +1,8 @@
 package com.parkinglot.api.controllers;
 
-import com.parkinglot.api.domain.ParkingBoy;
-import com.parkinglot.api.domain.ParkingBoyRepository;
-import com.parkinglot.api.domain.ParkingLot;
-import com.parkinglot.api.domain.ParkingLotRepository;
+import com.parkinglot.api.domain.*;
+import com.parkinglot.api.models.OrderResponse;
+import com.parkinglot.api.models.ParkingBoyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +11,9 @@ import javax.persistence.EntityManager;
 import java.net.*;
 import java.util.*;
 import java.util.stream.*;
+
+import static com.parkinglot.api.controllers.OrderResource.ORDER_STATUS_PARKING;
+import static com.parkinglot.api.controllers.OrderResource.ORDER_STATUS_PENDING;
 
 @RestController
 @RequestMapping("/parkingboys")
@@ -22,11 +24,16 @@ public class ParkingBoyResource {
     @Autowired
     private ParkingLotRepository parkingLotRepository;
     @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
     private EntityManager entityManager;
 
     @GetMapping
-    public ResponseEntity<String[]> testConnectionMethod() {
-        return ResponseEntity.ok(new String[] {"I", "am", "a", "new", "array"});
+    public ResponseEntity<ParkingBoyResponse[]> getAll() {
+        final ParkingBoyResponse[] parkingBoys = parkingBoyRepository.findAll().stream()
+                .map(ParkingBoyResponse::create)
+                .toArray(ParkingBoyResponse[]::new);
+        return ResponseEntity.ok(parkingBoys);
     }
 
     @PostMapping(consumes = {"application/json"})
@@ -35,5 +42,37 @@ public class ParkingBoyResource {
             return ResponseEntity.created(URI.create("/parkingboys/" + parkingBoy.getId())).build();
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping(value = "/{employeeId}/parkinglots/{parkingLotId}/orders/{orderId}")
+    public ResponseEntity<OrderResponse> parkCarToParkingLot(
+            @PathVariable Long employeeId,
+            @PathVariable Long parkingLotId,
+            @PathVariable Long orderId ) {
+
+//      uncomment after parkingboy sign up finished
+//        Optional<ParkingBoy> parkingBoy = parkingBoyRepository.findById(employeeId);
+//        if (!parkingBoy.isPresent()) {
+//            return ResponseEntity.status(404).header("errorMessage","parking boy id:"+orderId+" not found").build();
+//        }
+
+        Optional<ParkingLot> parkingLot = parkingLotRepository.findById(parkingLotId);
+        if (!parkingLot.isPresent()) {
+            return ResponseEntity.status(404).header("errorMessage","parking lot id:"+parkingLotId+" not found").build();
+        }
+
+        Optional<Order> order = orderRepository.findById(orderId);
+        if (!order.isPresent()) {
+            return ResponseEntity.status(404).header("errorMessage","parking order id:"+orderId+" not found").build();
+        }
+
+        if(!order.get().getOrderStatus().equals(ORDER_STATUS_PENDING)){
+            return ResponseEntity.status(400).header("errorMessage","parking order id:"+orderId+" status is not pending").build();
+        }
+
+        order.get().setOrderStatus(ORDER_STATUS_PARKING);
+        order.get().setParkingLotId(parkingLotId);
+
+        return ResponseEntity.ok(OrderResponse.create( order.get()));
     }
 }
