e28e4672e1445d7a170f5fc8968d9ffe978c1078
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-11 10:01:58 +0800

    [Mo] test: add test cases for OrderResource

--- src/test/java/com/parkinglot/api/OrderResourceTests.java
+++ src/test/java/com/parkinglot/api/OrderResourceTests.java
@@ -2,9 +2,12 @@ package com.parkinglot.api;
 
 import com.parkinglot.api.domain.Order;
 import com.parkinglot.api.domain.OrderRepository;
+import com.parkinglot.api.domain.ParkingBoy;
 import com.parkinglot.api.domain.ParkingBoyRepository;
+import com.parkinglot.api.domain.ParkingLot;
 import com.parkinglot.api.domain.ParkingLotRepository;
 import com.parkinglot.api.models.OrderResponse;
+import com.parkinglot.api.models.OrderTicketResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -61,6 +64,24 @@ public class OrderResourceTests {
     }
 
     @Test
+    public void should_get_orderTicket_based_on_order() throws Exception {
+        // Given
+        Order newOrder = orderRepository.save(new Order("Car1"));
+        orderRepository.flush();
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+            .get("/orders/" + newOrder.getId()))
+            .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+        final OrderTicketResponse orderTicketResponse = getContentAsObject(result, OrderTicketResponse.class);
+
+        assertEquals("Car1", orderTicketResponse.getVehicleNumber());
+    }
+
+    @Test
     public void should_create_new_order() throws Exception {
         // Given
         String vehicleNumber = "CarToAdd";
@@ -69,7 +90,7 @@ public class OrderResourceTests {
         final MvcResult createOrderResult = mvc.perform(MockMvcRequestBuilders
             .post("/orders")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"vehicleNumber\":\""+vehicleNumber+"\"}"))
+            .content("{\"vehicleNumber\":\"" + vehicleNumber + "\"}"))
             .andReturn();
 
         // When
@@ -85,4 +106,118 @@ public class OrderResourceTests {
         assertEquals(1, orderResponses.length);
         assertEquals(vehicleNumber, orderResponses[0].getVehicleNumber());
     }
+
+    @Test
+    public void should_update_status_from_pending_to_parking_and_assign_the_parking_boy_to_order() throws Exception {
+        // Given
+        Order newOrder = orderRepository.save(new Order("car"));
+        ParkingBoy newParkingBoy = parkingBoyRepository.save(new ParkingBoy("boy"));
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/employeeId/" + newParkingBoy.getId()))
+            .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+        final OrderResponse orderResponse = getContentAsObject(result, OrderResponse.class);
+
+        assertEquals("car", orderResponse.getVehicleNumber());
+        assertEquals("parking", orderResponse.getOrderStatus());
+        assertEquals(newParkingBoy.getId(), (long)orderResponse.getEmployeeId());
+    }
+
+    @Test
+    public void should_update_status_from_parking_to_parked_and_assign_the_parking_lot_id_to_order() throws Exception {
+        // Given
+        Order newOrder = orderRepository.save(new Order("car"));
+        ParkingBoy newParkingBoy = parkingBoyRepository.save(new ParkingBoy("boy"));
+        ParkingLot newParkingLot = parkingLotRepository.save(new ParkingLot("lot", 10));
+
+        // When
+        final MvcResult putEmployeeIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/employeeId/" + newParkingBoy.getId()))
+            .andReturn();
+
+        final MvcResult putParkingLotIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/parkingLotId/" + newParkingLot.getId()))
+            .andReturn();
+
+        // Then
+        assertEquals(200, putEmployeeIdResult.getResponse().getStatus());
+        assertEquals(200, putParkingLotIdResult.getResponse().getStatus());
+
+
+        final OrderResponse orderResponse = getContentAsObject(putParkingLotIdResult, OrderResponse.class);
+
+        assertEquals("car", orderResponse.getVehicleNumber());
+        assertEquals("parked", orderResponse.getOrderStatus());
+        assertEquals(newParkingBoy.getId(), (long)orderResponse.getEmployeeId());
+        assertEquals(newParkingLot.getId(), (long)orderResponse.getParkingLotId());
+    }
+
+    @Test
+    public void should_update_status_from_parked_to_parking() throws Exception {
+        // Given
+        Order newOrder = orderRepository.save(new Order("car"));
+        ParkingBoy newParkingBoy = parkingBoyRepository.save(new ParkingBoy("boy"));
+        ParkingLot newParkingLot = parkingLotRepository.save(new ParkingLot("lot", 10));
+
+        // When
+        final MvcResult putEmployeeIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/employeeId/" + newParkingBoy.getId()))
+            .andReturn();
+        final MvcResult putParkingLotIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/parkingLotId/" + newParkingLot.getId()))
+            .andReturn();
+        final MvcResult customerCreateFetchRequestResult = mvc.perform(MockMvcRequestBuilders
+            .patch("/orders/" + newOrder.getId()))
+            .andReturn();
+
+
+        // Then
+        assertEquals(200, putEmployeeIdResult.getResponse().getStatus());
+        assertEquals(200, putParkingLotIdResult.getResponse().getStatus());
+        assertEquals(200, customerCreateFetchRequestResult.getResponse().getStatus());
+
+        final OrderResponse orderResponse = getContentAsObject(customerCreateFetchRequestResult, OrderResponse.class);
+
+        assertEquals("car", orderResponse.getVehicleNumber());
+        assertEquals("fetching", orderResponse.getOrderStatus());
+    }
+
+    @Test
+    public void should_update_status_from_fetching_to_fetched() throws Exception {
+        // Given
+        Order newOrder = orderRepository.save(new Order("car"));
+        ParkingBoy newParkingBoy = parkingBoyRepository.save(new ParkingBoy("boy"));
+        ParkingLot newParkingLot = parkingLotRepository.save(new ParkingLot("lot", 10));
+
+        // When
+        final MvcResult putEmployeeIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/employeeId/" + newParkingBoy.getId()))
+            .andReturn();
+        final MvcResult putParkingLotIdResult = mvc.perform(MockMvcRequestBuilders
+            .put("/orders/" + newOrder.getId() + "/parkingLotId/" + newParkingLot.getId()))
+            .andReturn();
+        final MvcResult customerCreateFetchRequestResult = mvc.perform(MockMvcRequestBuilders
+            .patch("/orders/" + newOrder.getId()))
+            .andReturn();
+        final MvcResult parkingBoyFinishFetchResult = mvc.perform(MockMvcRequestBuilders
+            .delete("/orders/" + newOrder.getId()))
+            .andReturn();
+
+
+        // Then
+        assertEquals(200, putEmployeeIdResult.getResponse().getStatus());
+        assertEquals(200, putParkingLotIdResult.getResponse().getStatus());
+        assertEquals(200, customerCreateFetchRequestResult.getResponse().getStatus());
+        assertEquals(200, parkingBoyFinishFetchResult.getResponse().getStatus());
+
+        final OrderResponse orderResponse = getContentAsObject(parkingBoyFinishFetchResult, OrderResponse.class);
+
+        assertEquals("car", orderResponse.getVehicleNumber());
+        assertEquals("fetched", orderResponse.getOrderStatus());
+    }
+
 }
