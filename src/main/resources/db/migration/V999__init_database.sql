ef6c4b4a4bda3d92d9876a7db3978b489c675e3f
Mo Hon Sing <h.s.mo@oocl.com>
2018-12-14 11:24:48 +0800

    [Mo] fixed: update db rows

--- src/main/resources/db/migration/V999__init_database.sql
+++ src/main/resources/db/migration/V999__init_database.sql
@@ -1,8 +1,8 @@
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (0, 'Locki', 'locki999', 'locki@mail.com', 87562153, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG','WORKING');
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9994, 'Rex', 'rexlo', 'rexxx@mail.com', 68541257, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG','WORKING');
-INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9995, 'Jacky', 'jackyboy', 'jk@ma.com', 12357986, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'WORKING');
+INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9995, 'Jacky', 'jackyboy', 'jk@ma.com', 12357986, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'FROZEN');
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9996, 'Kate', 'kate', 'k@mail.com', 98563201, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'FROZEN');
-INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9997, 'Mo', 'mo', 'mo@mail.com', 95632501, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'FROZEN');
+INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9997, 'Mo', 'mohonsing', 'mo@mail.com', 95632501, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'WORKING');
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9998, 'admin', 'admin', 'admin@mail.com', 51245879, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'WORKING');
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (9999, 'human', 'human', 'human@mail.com', 15423568, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'WORKING');
 INSERT INTO employee (id, name, username, email, phone, password, status) VALUES (10000, 'manager', 'manager', 'manager@mail.com', 15268798, '$2a$10$DJIH.TaCpm/ILwW9yf937OZl2KvOCFOzkaSDScYGfH5cQA7kOPlFG', 'WORKING');
@@ -25,6 +25,7 @@ INSERT INTO parking_lot (id, parking_lot_name, capacity, reserved_space, employe
 INSERT INTO parking_lot (id, parking_lot_name, capacity, reserved_space, employee_id) VALUES (9005,'Grand ParkingLot', 10, 0, null);
 INSERT INTO parking_lot (id, parking_lot_name, capacity, reserved_space, employee_id) VALUES (9006,'Old Grand LOT', 5, 0, null);
 INSERT INTO parking_lot (id, parking_lot_name, capacity, reserved_space, employee_id) VALUES (9007,'New Lot besides Sea', 5, 0, null);
+INSERT INTO parking_lot (id, parking_lot_name, capacity, reserved_space, employee_id) VALUES (9008,'Mo ParkingLot', 5, 0, 9997);
 
 INSERT INTO parking_order (id, vehicle_number, order_status, parking_lot_id, employee_id) VALUES (99996, 35804146, 'parking', null, 0);
 INSERT INTO parking_order (id, vehicle_number, order_status, parking_lot_id, employee_id) VALUES (99997, 56383021, 'parked', 0, 0);
